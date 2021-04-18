a04a5a7d4b1c87bfdcf75d92a5e8674ca0371def
ipmp1921 indu <81903540+indraraj844@users.noreply.github.com>
2021-04-18 12:26:35 +0530

    Update check a string palindrome or not .cpp

--- check a string palindrome or not .cpp
+++ check a string palindrome or not .cpp
@@ -122,3 +122,105 @@ M1 using stack
     return 0;
     } 
  M2:
+by reverse last half and compare
+    #include<bits/stdc++.h>
+    using namespace std;
+
+    struct node{
+        char data;
+        node* next;
+    };
+  void push(struct node** root,char data)
+  {
+      node* temp=new node();
+      temp->data=data;
+      temp->next=(*root);
+
+      (*root)=temp;
+  }
+    void print_list(node* root)
+    {
+        if(!root)
+        {
+            cout<<" no element ";
+            return;
+        }
+        struct node* temp=root;
+        while(temp)
+        {
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+    int length(struct node* root)
+    {
+        node* temp=root;
+        int count=0;
+        while(temp)
+        {
+            count++;
+            temp=temp->next;
+        }
+        return count;
+    }
+    bool isPallindrome(struct node* root)
+    {
+        if(!(root))return true;
+        
+        node* slow=root;
+        node* fast=root;
+        while(fast&&fast->next)
+         {
+             slow=slow->next;
+             fast=fast->next->next;
+         }
+             node* curr;
+             int l=length(root);
+         if(l%2==0)
+         {
+             curr=slow;
+         }
+         else 
+           curr=slow->next;
+       
+        node* next=NULL;
+        node* prev=NULL;
+        while(curr)
+        {
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+
+      node* temp2=prev;
+      node* temp1=root;
+
+      while(temp2)
+      {
+          if(temp1->data!=temp2->data)
+          return false;
+          temp1=temp1->next;
+          temp2=temp2->next;
+      }
+      return true;
+    }
+    
+    int main()
+    {
+    node* a = NULL; 
+    push(&a, 'r'); 
+    push(&a, 'a'); 
+    push(&a, 'd'); 
+    push(&a, 'a'); 
+    push(&a, 'r'); 
+    if(isPallindrome(a))
+    cout<<" this is a pallindrome string";
+    else
+     cout<<" not pallindrome";
+    
+    return 0;
+    } 
+thank you
+
